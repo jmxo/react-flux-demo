a89cb0662b5899d6cbf75442839fd114e10f61f4
mohamed-ismat <mnb1236@live.com>
2017-03-24 19:01:24 +0300

    added header component and images gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -15,6 +15,7 @@ var config = {
     paths: {
       html: './src/*.html',
       js: './src/**/*.js',
+      images: './src/images/*',
       css: [
         'node_modules/bootstrap/dist/css/bootstrap.min.css',
         'node_modules/bootstrap/dist/css/bootstrap-theme.min.css'
@@ -64,6 +65,16 @@ gulp.task('css', function() {
       .pipe(gulp.dest(config.paths.dist + '/css'));
 });
 
+gulp.task('images', function() {
+  gulp.src(config.paths.images)
+    .pipe(gulp.dest(config.paths.dist + '/images'))
+    .pipe(connect.reload());
+
+    //favicon
+    gulp.src('./src/favicon.ico')
+      .pipe(gulp.dest(config.paths.dist));
+});
+
 gulp.task('lint', function() {
   return gulp.src(config.paths.js)
       .pipe(lint({config: '.eslintrc'}))
@@ -75,4 +86,4 @@ gulp.task('watch', function() {
   gulp.watch(config.paths.js, ['js', 'lint']);
 });
 
-gulp.task('default', ['html', 'js', 'css', 'lint', 'open', 'watch']);
+gulp.task('default', ['html', 'js', 'css', 'images', 'lint', 'open', 'watch']);
