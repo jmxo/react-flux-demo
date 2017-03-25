7eda4bdeebb88296d42806455add6bfbb30fb671
mohamed-ismat <mnb1236@live.com>
2017-03-26 01:43:49 +0300

    added typechecking with protoTypes

--- src/components/authors/authorPage.js
+++ src/components/authors/authorPage.js
@@ -4,7 +4,7 @@ var React = require('react');
 var AuthorApi = require('../../api/authorApi');
 var AuthorList = require('./authorList');
 
-var Authors = React.createClass({
+var AuthorPage = React.createClass({
   getInitialState: function() {
     return {
       authors: []
@@ -21,11 +21,11 @@ var Authors = React.createClass({
     return (
       <div>
         <h1>Authors</h1>
-        <AuthorList authors={this.state.authors} />
+        <AuthorList authors={this.state.authors}/>
       </div>
     );
   }
 
 });
 
-module.exports = Authors;
+module.exports = AuthorPage;
