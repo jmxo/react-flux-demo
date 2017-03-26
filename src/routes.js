a9b5747a8debd2de63e0eaec71f85b9ee4814854
mohamed-ismat <mnb1236@live.com>
2017-03-26 13:16:10 +0300

    added redirect route

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ var Router = require('react-router');
 var DefaultRoute = Router.DefaultRoute;
 var Route = Router.Route;
 var NotFoundRoute = Router.NotFoundRoute;
+var Redirect = Router.Redirect;
 
 var routes = (
     <Route name="app" path="/" handler={require('./components/app')}>
@@ -12,6 +13,7 @@ var routes = (
         <Route name="authors" handler={require('./components/authors/authorPage')}/>
         <Route name="about" handler={require('./components/about/aboutPage')}/>
         <NotFoundRoute handler={require('./components/NotFoundPage')} />
+        <Redirect from="about-us" to="about" /> //redirect from old urls
     </Route>
 );
 
