38a5ed3ae737007482095b4ed5701610b6830d79
mohamed-ismat <mnb1236@live.com>
2017-03-27 02:57:22 +0300

    setting up change listeners

--- src/components/authors/authorPage.js
+++ src/components/authors/authorPage.js
@@ -6,11 +6,28 @@ var Link = require('react-router').Link;
 var AuthorList = require('./authorList');
 var AuthorActions = require('../../actions/authorActions');
 var AuthorStore = require('../../stores/authorStore');
+
 var AuthorPage = React.createClass({
+
   getInitialState: function() {
     return {
       authors: AuthorStore.getAllAuthors()
     };
+  },
+
+  //hooking to store events
+  componentWillMount: function() {
+    AuthorStore.addChangeListener(this._onChange);
+  },
+
+  //clean up
+  componentWillUpdate: function() {
+    AuthorStore.removeChangeListener(this._onChange);
+  },
+
+
+  _onChange: function() { //to update state when the store changes
+    this.setState({ authors: AuthorStore.getAllAuthors() });
   },
 
   render: function() {
