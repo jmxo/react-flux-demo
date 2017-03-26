b0ee065748ccee95d6be65d236848632d38c4d60
mohamed-ismat <mnb1236@live.com>
2017-03-26 20:47:27 +0300

    added newAuthor components, handling state manually

--- src/components/about/aboutPage.js
+++ src/components/about/aboutPage.js
@@ -3,20 +3,20 @@
 var React = require('react');
 
 var About = React.createClass({
-	statics: {
-		willTransitionTo: function(transition, params, query, callback) {
-			if (!confirm('willTransitionTo demo: Are you sure you want to visit About page?')) {
-				transition.abort();
-			} else {
-				callback();
-			}
-		},
-		willTransitionFrom: function(transition, component) {
-			if (!confirm('willTransitionFrom demo: Are you sure you want to leave About page?')) {
-				transition.abort();
-			}
-		}
-	},
+	// statics: {
+	// 	willTransitionTo: function(transition, params, query, callback) {
+	// 		if (!confirm('willTransitionTo demo: Are you sure you want to visit About page?')) {
+	// 			transition.abort();
+	// 		} else {
+	// 			callback();
+	// 		}
+	// 	},
+	// 	willTransitionFrom: function(transition, component) {
+	// 		if (!confirm('willTransitionFrom demo: Are you sure you want to leave About page?')) {
+	// 			transition.abort();
+	// 		}
+	// 	}
+	// },
 	render: function () {
 		return (
 			<div>
