8cae36c0ea3b98e03709fec16212e4a3e5f56947
mohamed-ismat <mnb1236@live.com>
2017-03-27 01:54:43 +0300

    created author store

---
+++ src/stores/authorStore.js
@@ -0,0 +1,23 @@
+"use strict";
+
+var Dispatcher = require('../dispatcher/appDispatcher');
+var ActionTypes = require('../constants/actionTypes');
+var EventEmitter = require('events').EventEmitter;
+var assign = require('object-assign'); //sindre
+var CHANGE_EVENT = 'change';
+
+// mix all of this into a new object
+var AuthorStore = assign({}, EventEmitter.prototype, {
+  addChangeListener: function(callback) {
+    this.on(CHANGE_EVENT, callback);
+  },
+
+  removeChangeListener: function(callback) {
+    this.removeListener(CHANGE_EVENT, callback);
+  },
+
+  emitChange: function() {
+    this.emit(CHANGE_EVENT);
+  }
+
+});
