1ad492316ce93c8e52f0ac9cc699335f09958d9e
mohamed-ismat <mnb1236@live.com>
2017-03-26 12:49:52 +0300

    added routes and main component

---
+++ src/components/app.js
@@ -0,0 +1,21 @@
+/*eslint-disable strict */ //bcuz we need global avr for $
+
+var React = require('react');
+var Header = require('./common/header');
+var RouteHandler = require('react-router').RouteHandler;
+$ = jQuery = require('jquery');
+
+var App = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <Header />
+        <div className="container-fluid">
+          <RouteHandler />
+        </div>
+      </div>
+    );
+  }
+});
+
+module.exports = App;
