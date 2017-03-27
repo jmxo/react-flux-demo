1c71528d0a4591cc6d9338e6f89e456dfb1bb704
mohamed-ismat <mnb1236@live.com>
2017-03-27 03:20:46 +0300

    added deleteAuthor action

--- src/components/authors/authorList.js
+++ src/components/authors/authorList.js
@@ -3,11 +3,20 @@
 var React = require('react');
 var Router = require('react-router');
 var Link = Router.Link;
+var AuthorActions = require('../../actions/authorActions');
+var toastr = require('toastr');
 
 var AuthorList = React.createClass({
 
   propTypes: {
     authors: React.PropTypes.array.isRequired
+  },
+
+  // this could also have been done in the parent
+  deleteAuthor: function(id, event) {
+    event.preventDefault();
+    AuthorActions.deleteAuthor(id);
+    toastr.success('Author Deleted');
   },
 
   render: function() {
@@ -16,6 +25,7 @@ var AuthorList = React.createClass({
         <tr key={author.id}>
           <td><Link to="manageAuthor" params={{id: author.id}}>{author.id}</Link></td>
           <td>{author.firstName} {author.lastName}</td>
+          <td><a href="#" onClick={this.deleteAuthor.bind(this, author.id)}>Delete</a></td>
         </tr>
       );
     };
@@ -25,6 +35,7 @@ var AuthorList = React.createClass({
           <thead>
             <th>ID</th>
             <th>Name</th>
+            <th></th>
           </thead>
           <tbody>
             {this.props.authors.map(createAuthorRow, this)}
